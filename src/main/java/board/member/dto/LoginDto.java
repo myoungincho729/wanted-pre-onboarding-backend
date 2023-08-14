3e9558783bfef17b73ff12e89fe497ce1c7493da
myoungin <myoungin729@gmail.com>
2023-08-14 16:21:42 +0900

    feat: dto 검증 에너테이션 추가

--- src/main/java/board/member/dto/LoginDto.java
+++ src/main/java/board/member/dto/LoginDto.java
@@ -1,14 +1,25 @@
 package board.member.dto;
 
+import board.validator.EmailConstraint;
+import board.validator.PasswordConstraint;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.validation.annotation.Validated;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@Validated
 public class LoginDto {
+
+    @EmailConstraint
     private String email;
+    @PasswordConstraint
     private String password;
 
+    public LoginDto(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
 }
--- src/main/java/board/member/dto/MemberDto.java
+++ src/main/java/board/member/dto/MemberDto.java
@@ -1,5 +1,7 @@
 package board.member.dto;
 
+import board.validator.EmailConstraint;
+import board.validator.PasswordConstraint;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,7 +10,13 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class MemberDto {
+    @EmailConstraint
     private String email;
+    @PasswordConstraint
     private String password;
 
+    public MemberDto(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
 }
