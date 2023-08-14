9e4b278bf5a883b826440cc7824a2cda81562acd
myoungin <myoungin729@gmail.com>
2023-08-14 14:25:10 +0900

    feat: member 관련 dto 추가

---
+++ src/main/java/board/member/dto/MemberDto.java
@@ -0,0 +1,14 @@
+package board.member.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class MemberDto {
+    private String email;
+    private String password;
+
+}
---
+++ src/main/java/board/member/dto/MemberResponseDto.java
@@ -0,0 +1,19 @@
+package board.member.dto;
+
+import board.member.entity.Member;
+import lombok.Builder;
+
+public class MemberResponseDto {
+    private String email;
+
+    @Builder
+    public MemberResponseDto(String email) {
+        this.email = email;
+    }
+
+    public static MemberResponseDto fromMember(Member member) {
+        return MemberResponseDto.builder()
+                .email(member.getEmail())
+                .build();
+    }
+}
