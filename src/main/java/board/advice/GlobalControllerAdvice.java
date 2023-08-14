fe2b35299e31e6050302cf9e8d2b63b51b1c6336
myoungin <myoungin729@gmail.com>
2023-08-14 23:31:36 +0900

    feat: 공통 예외처리 클래스 정의, validation 에러처리 구현

--- src/main/java/board/advice/GlobalControllerAdvice.java
+++ src/main/java/board/advice/GlobalControllerAdvice.java
@@ -3,17 +3,30 @@ package board.advice;
 import board.response.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalControllerAdvice {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity validationFail(BindingResult bindingResult) {
+        int size = bindingResult.getFieldErrors().size();
+        System.out.println(size);
         Response response = Response.builder()
                 .error(bindingResult.getFieldError().getDefaultMessage())
+                .build();
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity validationFail(RuntimeException e) {
+        Response response = Response.builder()
+                .error(e.getMessage())
                 .build();
         return ResponseEntity.badRequest().body(response);
     }
