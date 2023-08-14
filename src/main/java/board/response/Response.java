e3538f823350401d1c3190a60fc7df74f2adf219
myoungin <myoungin729@gmail.com>
2023-08-14 17:45:38 +0900

    fix: getter 추가, 빌더 생성자 추가

--- src/main/java/board/response/Response.java
+++ src/main/java/board/response/Response.java
@@ -1,19 +1,17 @@
 package board.response;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class Response<T> {
     private String error;
     private T data;
 
+    @Builder
     public Response(String error, T data) {
         this.error = error;
         this.data = data;
     }
 
-    public Response error(String message) {
-        return new Response(message, null);
-    }
-
-    public Response data(T data) {
-        return new Response(null, data);
-    }
 }
